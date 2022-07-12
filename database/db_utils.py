f15138403807148a060f9e3e97b60d1644fb0331
TwistRU <roman.pavlyuk.02@mail.ru>
2022-07-13 01:10:23 +1000

    tmp load

--- database/db_utils.py
+++ database/db_utils.py
@@ -1,5 +1,28 @@
+import xlrd, openpyxl
+
+from utils import Data
+
+
 def load_tables_to_DB(files: list) -> list:
     """
     return Erros. If errors are empty, then load is successful.
+    files[0] - Таблица результатов мечения
+    files[1] - Таблица дат работ
+    files[2] - Таблица координат работ
     """
-    pass
+    columns_from_table = ["C", "D", "E", "F", "G", "H", "I", "J", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "AA", "AC", "AD", "AF", "AI", "AK", "AO", "AR", "AS", "CI"]
+    errors = []
+    result_table = openpyxl.open(files[0])
+    result_sheet = result_table.active
+    result_columns = [result_sheet[column_name] for column_name in columns_from_table]
+    result = list()
+    for i in range(len(result_sheet["A"])):
+        tmp = []
+        for column in result_columns:
+            tmp.append(column[i])
+        result.append(tuple(tmp))
+    Data.conn.executemany(f"""
+    insert into RESULT_TABLE values ("?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?");
+    """, result[1:])
+    Data.conn.commit()
+    return errors
--- database/setup.py
+++ database/setup.py
@@ -8,6 +8,73 @@ def setup_DB():
         Data.conn = connect("test.db")
     else:
         Data.conn = connect(":memory:")
+    define_db_tables()
 
-
+def define_db_tables():
+    """
+    C - ring -                      № кольца\n
+    D - date_of_capture_in_season - Дата отлова в сезон\n
+    E - date_of_tagging -           Дата мечения\n
+    F - returns -                   Возвраты\n
+    G - date_of_return -            Дата возврата\n
+    H - date_of_death -             Дата гибели\n
+    I - genus -                     Род\n
+    J - species -                   Вид\n
+    N - gender -                    Пол\n
+    O - age_EURING -                Возраст EURING\n
+    P - age -                       Возвраст\n
+    Q - pneumatization -            % пневматизации\n
+    R - richness -                  Балл жирности\n
+    S - muscle -                    Балл мышцы\n
+    T - weight -                    Вес\n
+    U - beak_from_forehead -        Клюв от лба\n
+    V - beak_from_nostril -         Ключ от ноздри\n
+    W - wing_min -                  Крыло min\n
+    X - wing_max -                  Крыло max\n
+    Y - tarsus -                    Цевка\n
+    AA - tail -                     Хвост\n
+    AC - head_length -              Длина головы\n
+    AD - mesh_number -              № сетки\n
+    AF - biotope_code -             Код биотопа\n
+    AI - mesh_footage -             Метраж сетей\n
+    AK - check_time -               Время проверки\n
+    AO - place_of_tagging -         Место мечения\n
+    AR - latitude_of_tagging -      Широта мечения\n
+    AS - longitude_of_tagging -     Долгото мечения\n
+    CI - molting_stage -            Стадия линьки\n
+    """
+    Data.conn.execute("""
+    CREATE TABLE IF NOT EXISTS RESULT_TABLE ( 
+        ring,
+        date_of_capture_in_season,
+        date_of_tagging,
+        returns,
+        date_of_return,
+        date_of_death,
+        genus,
+        species,
+        gender,
+        age_EURING,
+        age,
+        pneumatization,
+        richness,
+        muscle,
+        weight,
+        beak_from_forehead,
+        beak_from_nostril,
+        wing_min,
+        wing_max,
+        tarsus,
+        tail,
+        head_length,
+        mesh_number,
+        biotope_code,
+        mesh_footage,
+        check_time,
+        place_of_tagging,
+        latitude_of_tagging,
+        longitude_of_tagging,
+        molting_stage
+    )
+    """)
 
