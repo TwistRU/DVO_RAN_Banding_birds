5a1272c4acf2bc8327f159159f9b310a8066d495
s7rife <dimonchik22824@gmail.com>
2022-07-14 21:35:15 +0300

    Widget for time series of primary numbers table

--- mode_dict.py
+++ mode_dict.py
@@ -5,6 +5,7 @@ from PyQt6.QtWidgets import QWidget
 from widget.widget_morphology import MorphologyWidget
 from widget.widget_in_seasonal_history_2 import InSeasonalHistory2Widget
 from widget.widget_in_seasonal_history import InSeasonalHistoryWidget
+from widget.widget_time_series_of_primary_numbers import TimeSeriesOfPrimaryNumbersWidget
 
 
 @dataclass
@@ -16,7 +17,7 @@ class Mode:
 
 
 MODE_DICT = [
-    Mode(1, None, "Временные ряды перв. и отн. численностей", ""),
+    Mode(1, TimeSeriesOfPrimaryNumbersWidget, "Временные ряды перв. и отн. численностей", ""),
     Mode(2, None, "Временные ряды накопленной численности", ""),
     Mode(3, None, "Внутрисезонная история отловов кольца", ""),
     Mode(4, InSeasonalHistoryWidget, "Межсезонная история отловов кольца", ""),
---
+++ widget/widget_time_series_of_primary_numbers.py
@@ -0,0 +1,39 @@
+from PyQt6 import QtWidgets, uic
+from PyQt6.QtWidgets import QWidget, QLabel, QPushButton
+from database.db_reader import get_unique_columns_values, get_data_by_columns
+from widget.widget_ext_combo_box import ExtendedComboBox
+
+
+class TimeSeriesOfPrimaryNumbersWidget(QWidget):
+
+    def __init__(self):
+        super(TimeSeriesOfPrimaryNumbersWidget, self).__init__()
+        uic.loadUi('ui/time_series_of_primary_numbers.ui', self)
+        self.data = []
+
+        self.label_found: QLabel = self.findChild(QtWidgets.QLabel, 'label_found')
+        self.push_button_find: QPushButton = self.findChild(QtWidgets.QPushButton, 'push_button_find')
+
+        self.combo_box_species: ExtendedComboBox = self.findChild(ExtendedComboBox, 'combo_box_species')
+        self.combo_box_gender: ExtendedComboBox = self.findChild(ExtendedComboBox, 'combo_box_gender')
+        self.combo_box_age: ExtendedComboBox = self.findChild(ExtendedComboBox, 'combo_box_age')
+
+        self.combo_box_species.addItems(sorted(get_unique_columns_values(["genus", "species"])))
+        self.combo_box_gender.addItems(sorted(get_unique_columns_values(["gender"])))
+        self.combo_box_age.addItems(get_unique_columns_values(["age"]))
+
+        self.combo_box_species.setCurrentText("")
+        self.combo_box_gender.setCurrentText("")
+        self.combo_box_age.setCurrentText("")
+
+        self.push_button_find.clicked.connect(self.find_btn_clicked)
+
+    def find_btn_clicked(self):
+        self.data = get_data_by_columns([("genus_and_species", self.combo_box_species.currentText()),
+                                         ("gender", self.combo_box_gender.currentText()),
+                                         ("age", self.combo_box_age.currentText())])
+
+        self.label_found.setText(f'Найдено: {len(self.data) - 1}')
+
+    def get_results(self):
+        return self.data
