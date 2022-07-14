5184f32b0acc915b067a51b19d4eb80b7224ecca
s7rife <dimonchik22824@gmail.com>
2022-07-14 21:53:27 +0300

    Widget for time series of accumulated numbers table

--- mode_dict.py
+++ mode_dict.py
@@ -6,6 +6,7 @@ from widget.widget_morphology import MorphologyWidget
 from widget.widget_in_seasonal_history_2 import InSeasonalHistory2Widget
 from widget.widget_in_seasonal_history import InSeasonalHistoryWidget
 from widget.widget_time_series_of_primary_numbers import TimeSeriesOfPrimaryNumbersWidget
+from widget.widget_time_series_of_accumulated_numbers import TimeSeriesOfAccumulatedNumbersWidget
 
 
 @dataclass
@@ -18,7 +19,7 @@ class Mode:
 
 MODE_DICT = [
     Mode(1, TimeSeriesOfPrimaryNumbersWidget, "Временные ряды перв. и отн. численностей", ""),
-    Mode(2, None, "Временные ряды накопленной численности", ""),
+    Mode(2, TimeSeriesOfAccumulatedNumbersWidget, "Временные ряды накопленной численности", ""),
     Mode(3, None, "Внутрисезонная история отловов кольца", ""),
     Mode(4, InSeasonalHistoryWidget, "Межсезонная история отловов кольца", ""),
     Mode(5, InSeasonalHistory2Widget, "Внутрисезонная история отловов (на выбор)", ""),
